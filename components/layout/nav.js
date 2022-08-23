e91888c6a10c15f5b86336357a271d6e785b2c5f
Andreas <74594568+Relouding@users.noreply.github.com>
2022-08-23 13:31:24 +0200

    first push

---
+++ components/layout/nav.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Nav({ children }) {
+  return (
+    <>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/hotel-results">
+          <a>Hotels</a>
+        </Link>
+        <Link href="/contact">
+          <a>Contact</a>
+        </Link>
+      </nav>
+      <div className="container">{children}</div>
+    </>
+ );
+}
---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Nav from "../components/layout/nav";
+
+export default function Contact() {
+  return (
+    <Nav>
+      <Head />
+        <div className="container">
+          <h1>Contact page</h1>
+        </div>
+    </Nav>
+ );
+}
