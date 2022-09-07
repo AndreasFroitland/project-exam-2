8cc1e448fe74c0db17b3e147b66a5ba57136b4aa
Andreas <74594568+Relouding@users.noreply.github.com>
2022-09-07 22:50:59 +0200

    Push

--- components/form/formError.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-export default function ValidationError({ children }) {
-	return <div className="form-error">{children}</div>;
-}
-
-ValidationError.proptTypes = {
-	children: PropTypes.node.isRequired,
-};
