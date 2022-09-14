cafabf1771535ffd52b871aca31b8bb32c3fa924
Andreas <74594568+Relouding@users.noreply.github.com>
2022-09-14 12:17:32 +0200

    Fourteenth push

--- src/components/layout/StyleEstablishments.jsx
+++ src/components/layout/StyleEstablishments.jsx
@@ -5,13 +5,14 @@ export const ContainerEstablishments = styled.div`
     display: inline-block;
     max-width: 1200px;
     margin: 0 auto;
-    padding-right: .5rem;
+    margin-right: .5rem;
+    background-color: #FFF;
+    border-radius: .5rem;
     @media (max-width: 750px) {
         display: block;
-        padding-right: 0;
-    }
-    @media (max-width: 750px) {
-        width: 95%;
+        width: 100%;
+        margin: 0 auto;
+        margin-bottom: 1rem;
     }
 `;
 
@@ -19,6 +20,8 @@ export const StyleEstablishments = styled.div`
     max-width: 300px;
     display: inline-block;
     vertical-align: middle;
+    height: 150px;
+    padding: .5rem;
     }
     h1 {
         font-family: sans-serif;
@@ -33,23 +36,31 @@ export const StyleEstablishments = styled.div`
         margin: 0;
         padding-left: .5rem;
     }
-    @media (max-width: 1155px) {
+    @media (max-width: 1190px) {
         display: block;
     }
     @media (max-width: 750px) {
-        width: 100%;
+        max-width: 95%;
+        margin: 0 auto;
+        padding-top: .5rem;
+        padding-bottom: .5rem;
     }
 `;
 
 export const StyleImage = styled.img`
     display: inline-block;
-    padding: .5rem;
-    width: 250px;
-    height: 180px;
+    width: 300px;
+    height: 150px;
     margin: 0 auto;
     vertical-align: middle;
     object-fit: cover;
+    object-position: 50% 50%;
     overflow: hidden;
+    border-radius: .5rem;
+    @media (max-width: 750px) {
+        width: 100%;
+        padding: 0;
+    }
 `;
 
 export const StyleRating = styled.p`
